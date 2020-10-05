e4879fac427695048231a073c6834c99a25f1b35
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-05 16:54:44 -0700

    test PASSED

--- product-utils.js
+++ product-utils.js
@@ -4,12 +4,43 @@
 // year: '1997',
 // image: 'princess.png',
 // price: 665000,
-// price: 665000,
-// description: 'Released
+// description: 'Princess the Bear is...
 // catagory: 'collectable'
 
 export function renderBeanie(beanie) {
+    const li = document.createElement('li');
+    const name = document.createElement('p');
+    const year = document.createElement('p');
+    const image = document.createElement('img');
+    const price = document.createElement('p');
+    const description = document.createElement('div');
+    const button = document.createElement('button');
+    
+    li.classList.add('beanie');
 
+    name.classList.add('name');
+    name.textContent = beanie.name;
+    li.appendChild(name);
 
-    return '';
+    year.classList.add('year');
+    year.textContent = beanie.year;
+    li.appendChild(year);
+    
+    price.classList.add('price');
+    price.textContent = `${beanie.price.toLocaleString('en-US', { style:'currency', currency:'USD' })}`;
+    li.appendChild(price);
+ 
+    image.classList.add('image');
+    image.src = `../assets/${beanie.image}`;
+    image.alt = beanie.name;
+    li.appendChild(image);
+
+    description.classList.add('description');
+    description.textContent = beanie.description;
+    li.appendChild(description);
+
+    button.textContent = 'Add to cart';
+    li.appendChild(button);
+
+    return li;
 }
