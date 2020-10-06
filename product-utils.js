6a84d2c06d76eef1fa83ada8d46cdc1c37e4a4a4
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-05 18:54:33 -0700

    add data to product-data.js

--- product-utils.js
+++ product-utils.js
@@ -7,7 +7,7 @@
 // description: 'Princess the Bear is...
 // catagory: 'collectable'
 
-export function renderBeanie(beanie) {
+export function renderBeanie(beanies) {
     const li = document.createElement('li');
     const name = document.createElement('p');
     const year = document.createElement('p');
@@ -19,24 +19,24 @@ export function renderBeanie(beanie) {
     li.classList.add('beanie');
 
     name.classList.add('name');
-    name.textContent = beanie.name;
+    name.textContent = beanies.name;
     li.appendChild(name);
 
     year.classList.add('year');
-    year.textContent = beanie.year;
+    year.textContent = beanies.year;
     li.appendChild(year);
     
     price.classList.add('price');
-    price.textContent = `${beanie.price.toLocaleString('en-US', { style:'currency', currency:'USD' })}`;
+    price.textContent = `${beanies.price.toLocaleString('en-US', { style:'currency', currency:'USD' })}`;
     li.appendChild(price);
  
     image.classList.add('image');
-    image.src = `../assets/${beanie.image}`;
-    image.alt = beanie.name;
+    image.src = `../assets/${beanies.image}`;
+    image.alt = beanies.name;
     li.appendChild(image);
 
     description.classList.add('description');
-    description.textContent = beanie.description;
+    description.textContent = beanies.description;
     li.appendChild(description);
 
     button.textContent = 'Add to cart';
--- test/product.test.js
+++ test/product.test.js
@@ -4,24 +4,24 @@ import { renderBeanie } from '../product-utils.js';
 const test = QUnit.test;
 
 test('should take in a beanie and retun an li with the appropariate contents', (expect) => {
-    const beanie = {
+    const beanies = {
         id: 'princess',
-        name: 'Princess The Bear',
+        name: 'Princess the Bear',
         year: '1997',
         image: 'bb-princess.png',
         price: 665000,
-        description: 'Princess the Bear is considered the Holy Grail of their Beanie Baby collection. The a purple bear with a white rose embroidered on its chest, was intended to be a fundraising model for The Diana, Princess of Wales Memorial Fund. Because of its rarity, the Princess the Bear Beanie baby model is considered to be the most expensive and valuable Beanie Baby.Released in 1997, after the Beanie Babies craze had already hit, Princess the Bear was not part of the original collection.',
+        description: 'Princess the Bear is considered the Holy Grail of their Beanie Baby collection. The a purple bear with a white rose embroidered on its chest, was intended to be a fundraising model for The Diana, Princess of Wales Memorial Fund.',
         catagory: 'collectable',
     };
     
     
     //Arrange
     // Set up your arguments and expectations
-    const expected = '<li class="beanie"><p class="name">Princess The Bear</p><p class="year">1997</p><p class="price">$665,000.00</p><img class="image" src="../assets/bb-princess.png\" alt="Princess The Bear"><div class="description">Princess the Bear is considered the Holy Grail of their Beanie Baby collection. The a purple bear with a white rose embroidered on its chest, was intended to be a fundraising model for The Diana, Princess of Wales Memorial Fund. Because of its rarity, the Princess the Bear Beanie baby model is considered to be the most expensive and valuable Beanie Baby.Released in 1997, after the Beanie Babies craze had already hit, Princess the Bear was not part of the original collection.</div><button>Add to cart</button></li>';
+    const expected = '<li class="beanie"><p class="name">Princess the Bear</p><p class="year">1997</p><p class="price">$665,000.00</p><img class="image" src="../assets/bb-princess.png\" alt="Princess The Bear"><div class="description">Princess the Bear is considered the Holy Grail of their Beanie Baby collection. The a purple bear with a white rose embroidered on its chest, was intended to be a fundraising model for The Diana, Princess of Wales Memorial Fund.</div><button>Add to cart</button></li>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderBeanie(beanie);
+    const actual = renderBeanie(beanies);
 
     //Expect
     // Make assertions about what is expected versus the actual result
