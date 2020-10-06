a5badb5cacd2e1654204b4cc3b59c4db60372c77
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-06 14:43:27 -0700

    add renderTableRow function to cart-utils.js

--- cart/cart-utils.js
+++ cart/cart-utils.js
@@ -1,9 +1,29 @@
-export function findById(someArray, someId) {
-    for (let i = 0; i < someArray.length; i++) {
-        const item = someArray[i];
-        // is this item a match with our someId?
-        if (item.id === someId) {
-            return item;
-        }
-    }
+import { beanies } from '../product-data.js';
+import { findById } from '../product-utils.js';
+
+export function renderTableRow(cartItem) {
+    const tr = document.createElement('tr');
+    const tdName = document.createElement('td');
+    const tdPrice = document.createElement('td');
+    const tdQuantity = document.createElement('td');
+    const tdSubTotal = document.createElement('td');
+
+    tdQuantity.textContent = cartItem.quantity;
+
+    const beanieData = findById(beanies, cartItem.id);
+
+    /* bookData is an object that has: .title, .id, .price, .author */
+    const price = beanieData.price;
+    const name = beanieData.name;
+
+    tdPrice.textContent = `$${price}`;
+    tdName.textContent = name;
+
+    const total = price * cartItem.quantity;
+
+    tdSubTotal.textContent = `$${total}`;
+
+    tr.append(tdName, tdPrice, tdQuantity, tdSubTotal);
+
+    return tr;
 }
