3c76f333681228401aa395105a82dbd14618d3e2
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-06 14:36:38 -0700

    move findById function to utils.js

--- product-utils.js
+++ product-utils.js
@@ -6,6 +6,15 @@
 // price: 665000,
 // description: 'Princess the Bear is...
 // catagory: 'collectable'
+export function findById(someArray, someId) {
+    for (let i = 0; i < someArray.length; i++) {
+        const item = someArray[i];
+        // is this item a match with our someId?
+        if (item.id === someId) {
+            return item;
+        }
+    }
+}
 
 export function renderBeanie(beanies) {
     const li = document.createElement('li');
