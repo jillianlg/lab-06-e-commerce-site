c567bbb34eead3d02bfe28ad6323e8bd152b3cff
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-06 21:42:02 -0700

    update css styles

--- cart/cart.js
+++ cart/cart.js
@@ -1,7 +1,7 @@
 import { cartTotal, renderTableRow } from '../cart/cart-utils.js';
 import { cart } from '../cart/cart-data.js';
 import { beanies } from '../product-data.js';
-import { findById } from '../product-utils.js';
+// import { findById } from '../product-utils.js';
 
 const table = document.querySelector('tbody');
 
